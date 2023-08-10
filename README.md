5b14d1763b65f2438e77182012ec85ec31a3740a
SungchulCha <tony4907813@gmail.com>
2023-08-11 08:39:05 +0900

    헤더 정리 중. Todo: 코드 분석, 헤더 정리 완료할것

--- README.md
+++ README.md
@@ -2,6 +2,37 @@
 
 ### 리눅스 동작
 
+헤더파일
+
+<unistd.h>
+
+```c
+int close(int fd);
+ssize_t write(int fd, const void * buf, size_t nbytes);
+ssize_t read(int fd, void *buf,size_t nbytes);
+```
+
+<arpa/inet.h> : 주소 변환 기능을 사용할 경우 사용하는 헤더
+
+<sys/types.h> : 데이터 r/w 을 위한 파일 I/O, 자료형이 저장된 헤더
+해당 코드는 없어도 돌아감
+
+```c
+int open(const char *path, int flag);
+```
+
+<sys/socekt.h> : 소켓 헤더
+
+```c
+int socket(int domain,int type, int protocol);
+int bind(int sockfd, struct sockaddr, *myaddr, socklen_t addrlen);
+int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+int listen(int sockfd, int backlog);
+int connect(int sockfd, struct sockaddr *serv_addr,socklen_t addrlen);
+```
+
+<netinet/in.h> : 소켓 구조체 헤더
+
 ### TCP/IP 통신
 
 ### thread 사용
--- chatting_server.c
+++ chatting_server.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
@@ -20,106 +19,110 @@ int g_clnt_count = 0;
 
 pthread_mutex_t g_mutex;
 
-void sendAllClnt(char *msg, int mysock) {
+void sendAllClnt(char *msg, int mysock)
+{
 
-	pthread_mutex_lock(&g_mutex);
+  pthread_mutex_lock(&g_mutex);
 
-	for(int i = 0; i < g_clnt_count; i++) {
-		if(g_clnt_sockets[i] != mysock) {
-			printf("send msg : %s", msg);
-			write(g_clnt_sockets[i], msg, strlen(msg) + 1);
-		}
-	}	
+  for (int i = 0; i < g_clnt_count; i++)
+  {
+    if (g_clnt_sockets[i] != mysock)
+    {
+      printf("send msg : %s", msg);
+      write(g_clnt_sockets[i], msg, strlen(msg) + 1);
+    }
+  }
 
-	pthread_mutex_unlock(&g_mutex);
+  pthread_mutex_unlock(&g_mutex);
 }
 
-void* clnt_connection(void* arg) {
+void *clnt_connection(void *arg)
+{
 
-	int clnt_sock = (int)arg;
-	int str_len = 0;
+  int clnt_sock = (int)arg;
+  int str_len = 0;
 
-	char msg[BUFFSIZE];
-	int i;
+  char msg[BUFFSIZE];
+  int i;
 
-	while(1) {
+  while (1)
+  {
 
-		str_len = read(clnt_sock, msg, sizeof(msg));
-		if(str_len == -1) {
-			printf("clnt[%d] close\n", clnt_sock);
-			break;
-		}
-		sendAllClnt(msg, clnt_sock);
-		
-		printf("%s\n", msg);
+    str_len = read(clnt_sock, msg, sizeof(msg));
+    if (str_len == -1)
+    {
+      printf("clnt[%d] close\n", clnt_sock);
+      break;
+    }
+    sendAllClnt(msg, clnt_sock);
 
-	}
+    printf("%s\n", msg);
+  }
 
-	pthread_mutex_lock(&g_mutex);
+  pthread_mutex_lock(&g_mutex);
 
-	for(i = 0; i < g_clnt_count; i++) {
-		if(clnt_sock == g_clnt_sockets[i]) {
-			for(; i < g_clnt_count - 1; i++)
-				g_clnt_sockets[i] = g_clnt_sockets[i + 1];
-			break;
-		}
-	}
+  for (i = 0; i < g_clnt_count; i++)
+  {
+    if (clnt_sock == g_clnt_sockets[i])
+    {
+      for (; i < g_clnt_count - 1; i++)
+        g_clnt_sockets[i] = g_clnt_sockets[i + 1];
+      break;
+    }
+  }
 
-	pthread_mutex_lock(&g_mutex); //unlock으로 바꿔보기
-	close(clnt_sock);
-	pthread_exit(0);
-	return NULL;
+  pthread_mutex_lock(&g_mutex); // unlock으로 바꿔보기
+  close(clnt_sock);
+  pthread_exit(0);
+  return NULL;
 }
 
+int main(int argc, char **argv)
+{
 
-int main(int argc, char** argv) {
+  int serv_sock;
+  int clnt_sock;
 
-	int serv_sock;
-	int clnt_sock;
+  pthread_t t_thread;
 
+  struct sockaddr_in clnt_addr;
+  int clnt_addr_size;
 
-	pthread_t t_thread;
+  pthread_mutex_init(&g_mutex, NULL);
 
-	struct sockaddr_in clnt_addr;
-	int clnt_addr_size;
+  struct sockaddr_in serv_addr;
+  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(PORT);
+  //	serv_addr.sin_port = htons(atoi(argv[1]);
 
-	pthread_mutex_init(&g_mutex, NULL);
+  if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+  {
+    printf("bind error\n");
+  }
 
-	struct sockaddr_in serv_addr;
-	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-	
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(PORT);
-//	serv_addr.sin_port = htons(atoi(argv[1]);
+  if (listen(serv_sock, 5) == -1)
+  {
+    printf("listen error");
+  }
 
-	if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
-		printf("bind error\n");
-	}
+  char buff[200];
+  int recv_len = 0;
 
-	if(listen(serv_sock, 5) == -1) {
-		printf("listen error");
-	}
+  while (1)
+  {
 
-	char buff[200];
-	int recv_len = 0;
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
 
-	while(1) {
+    pthread_mutex_lock(&g_mutex);
+    g_clnt_sockets[g_clnt_count++] = clnt_sock;
+    pthread_mutex_unlock(&g_mutex);
 
-		clnt_addr_size = sizeof(clnt_addr);
-		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    pthread_create(&t_thread, NULL, clnt_connection, (void *)clnt_sock);
+  }
 
-		pthread_mutex_lock(&g_mutex);
-		g_clnt_sockets[g_clnt_count++] = clnt_sock;
-		pthread_mutex_unlock(&g_mutex);
-
-		pthread_create(&t_thread, NULL, clnt_connection, (void*)clnt_sock);
-
-
-	}
-
-
-
-	return 0;
+  return 0;
 }
